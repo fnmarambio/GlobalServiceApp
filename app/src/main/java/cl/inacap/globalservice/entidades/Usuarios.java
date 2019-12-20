e418e0f2cad5d6f01a282f9cdd11362e6b6fd786
Rekerhido <diegosebastiantl19@gmail.com>
2019-12-20 16:55:21 -0300

    primary key

--- app/src/main/java/cl/inacap/globalservice/entidades/Usuarios.java
+++ app/src/main/java/cl/inacap/globalservice/entidades/Usuarios.java
@@ -9,4 +9,70 @@ public class Usuarios {
     private String correo;
     private String contrasenaUser;
     private String paisUser;
+
+    public Usuarios(Integer idUser, String nombreUser, String apellidoUser, String rutUser, String correo, String contrasenaUser, String paisUser) {
+        this.idUser = idUser;
+        this.nombreUser = nombreUser;
+        this.apellidoUser = apellidoUser;
+        this.rutUser = rutUser;
+        this.correo = correo;
+        this.contrasenaUser = contrasenaUser;
+        this.paisUser = paisUser;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNombreUser() {
+        return nombreUser;
+    }
+
+    public void setNombreUser(String nombreUser) {
+        this.nombreUser = nombreUser;
+    }
+
+    public String getApellidoUser() {
+        return apellidoUser;
+    }
+
+    public void setApellidoUser(String apellidoUser) {
+        this.apellidoUser = apellidoUser;
+    }
+
+    public String getRutUser() {
+        return rutUser;
+    }
+
+    public void setRutUser(String rutUser) {
+        this.rutUser = rutUser;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenaUser() {
+        return contrasenaUser;
+    }
+
+    public void setContrasenaUser(String contrasenaUser) {
+        this.contrasenaUser = contrasenaUser;
+    }
+
+    public String getPaisUser() {
+        return paisUser;
+    }
+
+    public void setPaisUser(String paisUser) {
+        this.paisUser = paisUser;
+    }
 }
